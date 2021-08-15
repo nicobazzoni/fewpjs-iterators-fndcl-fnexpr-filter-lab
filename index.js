11af0d026fcbe4764e385f01f24b695f68c1d533
nicobazzoni <jillianamagsila@Jillians-iMac.fios-router.home>
2021-08-14 21:53:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(driver, string) {
+    return driver.filter( maybe =>
+      maybe.toLowerCase() === string.toLowerCase()
+    )
+  }
+  
+  function fuzzyMatch(driver, arg) {
+    return driver.filter(maybe=>
+      maybe.toLowerCase().indexOf(arg.toLowerCase()) === 0
+    )
+  }
+  
+  function matchName(driver, wanted) {
+    return driver.filter( result => result.name === wanted)
+  }
